05318e836c79998997d642e660c0857ddbab7752
Niccolò Califano <niccolo.califano19@gmail.com>
2026-05-19 11:30:33 -0400

    moved .pyz generation to dist/ folder. added removal of previous .pyz file

--- pack.sh
+++ pack.sh
@@ -1,4 +1,6 @@
 # pack everything into a single executable .pyz
 # to keep the code modular in multiple files during development but
-# copy only a single file on the target machine 
-python -m zipapp ./ -m "privesc:main" -o privesc.pyz
+# copy only a single file on the target machine
+rm ./dist/privesc.pyz
+
+python -m zipapp ./ -m "privesc:main" -o ./dist/privesc.pyz
